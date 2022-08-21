56588de42470a5efd062e68b4df0d25c1a9eaf88
TJ <tjthejuggler@gmail.com>
2022-08-21 21:00:59 +0300

    first commit

---
+++ properties.mk
@@ -0,0 +1,4 @@
+DEVICE = fenix6xpro
+SDK_HOME = /home/lunkwill/.Garmin/ConnectIQ/Sdks/connectiq-sdk-lin-4.1.5-2022-08-03-6e17bf167
+DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
+PRIVATE_KEY = /home/lunkwill/projects/garmin_dev_key/developer_key
